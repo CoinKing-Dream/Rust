1ff6ff68bb3a5e39330ed0b1800d7aaa45669ee1
MitsuiMinoru <mitsuimino711@gamil.com>
2024-03-26 16:42:03 +1000

    second

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,50 @@
 use rand::Rng;
 fn main(){
+    let day = String::from("Welcome");
     let random_number = rand::thread_rng().gen_range(1..=10);
     println!("Generate Random Number 1 and 10: {}", random_number);
+    
+    enum WEEKEND{
+        SUNDAY,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY
+    }
+
+    let date = 5;
+
+    match date{
+        1..=4 => println!("{}", date),
+        5 | 6 => println!("wrong"),
+        _ => println!("Invalid!")
+
+    }
+
+    match day.as_str() {
+        "Welcome" => {
+            println!("Welcome!");
+            // Additional logic for "Welcome" case
+        },
+        _ => {
+            println!("Invalid day: {}", day);
+            // Additional logic for other cases
+        }
+    }
+
+    let weekend = WEEKEND::SUNDAY;
+
+    match weekend {
+        WEEKEND::SUNDAY | WEEKEND::SATURDAY => {
+            println!("OK");
+        }
+        _ => {
+            println!("Invalid weekday");
+        }
+
+    }
+
+
 }

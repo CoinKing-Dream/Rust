df0243a3d034f7d55b6f901980cf1d9622cfa771
MitsuiMinoru <mitsuimino711@gamil.com>
2024-04-04 15:09:26 +1000

    Update 0404

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use rand::Rng;
+use std::env;
+
 fn main(){
     let day = String::from("Welcome");
     let random_number = rand::thread_rng().gen_range(1..=10);
@@ -52,5 +54,36 @@ fn main(){
     println!("{}", Number);
     println!("{}", Floating_Number);
 
+    let array = [1,2,3,4,5];
 
+    for  item in array{
+        println!("{}", item);
+    }
+
+    for item in 1..=10 {
+        println!("{}", item);
+    }
+
+    let numbers: &[i32] = &vec![1, 2, 3, 4, 5, 6];
+
+    for (i, item) in numbers.iter().enumerate() {
+        println!("value => {},  Index => {}", item, i);
+    }
+
+    
+    let add = |first: u32, second: u32| -> u32 {
+        return first;
+    };
+    
+    println!("{}", add(12, 25));
+
+    // let employ: (u32, &str, bool) = (1, "john", true);
+
+    // let args: Vec<String> = env::args().collect();
+
+    // let name = &args[1];
+    // let filename = &args[2];
+
+    // println!("Name is {}", name);
 }
+
